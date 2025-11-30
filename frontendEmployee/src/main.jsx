24ce460956420540a8cceb04e163d803485721ca
YashaswiniMJ1319 <mjyr1319@gmail.com>
2025-11-30 11:24:21 +0530

    correct

--- frontendEmployee/src/main.jsx
+++ frontendEmployee/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthContext.jsx";
+import "./index.css"; // <-- REQUIRED TO APPLY TAILWIND
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
